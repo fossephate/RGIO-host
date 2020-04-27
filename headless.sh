ae1f01048f09882bad306c4ab9094e7615a4146f
Fosse <matt.cfosse@gmail.com>
2020-04-26 20:26:46 -0400

    disable shm memory on chrome

--- headless.sh
+++ headless.sh
@@ -1,4 +1,7 @@
 Xvfb :1 -screen 0 1920x1080x16 &
 openbox-session &
-chromium-browser &
-node --experimental-modules ./src/libs/lagless/HostStream.mjs --user="fosse5" --password="QWERTY1234" --drawMouse="true" --usePulse="true" --audioDevice="default" --useLocalFfmpegInstall="true"
+# https://stackoverflow.com/questions/56218242/headless-chromium-on-docker-fails
+chromium-browser --disable-dev-shm-usage &
+node --experimental-modules ./src/libs/lagless/HostStream.mjs --user="fosse5" --password="QWERTY1234" \
+--drawMouse="true" --usePulse="true" \
+--audioDevice="default" --useLocalFfmpegInstall="true"
